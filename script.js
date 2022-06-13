77e1272742a57dfa13f85598b945a0c14675c77b
Angelotravain <84361149+Angelotravain@users.noreply.github.com>
2022-06-13 13:51:13 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 
-function Gerarqrcode(){
-    var inputUsuario = document.querySelector('textarea').value;
-    var GoogleChartAPI = 'https://chart.googleapis.com/chart?cht=qr&chs=200x200&chl='
-    var Conteudoqrcode = GoogleChartAPI + inputUsuario;
-    document.querySelector('#exitqrcode').src = Conteudoqrcode;
+function Gerarqrcode() {
+    if (document.querySelector('#optionsDensity').value === '1') {
+        var inputUsuario = document.querySelector('textarea').value;
+        var GoogleChartAPI = 'https://chart.googleapis.com/chart?cht=qr&chs=200x200&chld=M&chl='
+        var Conteudoqrcode = GoogleChartAPI + encodeURI(inputUsuario);
+        document.querySelector('#exitqrcode').src = Conteudoqrcode;
+    } else if (document.querySelector('#optionsDensity').value === '2') {
+        var inputUsuario = document.querySelector('textarea').value;
+        var GoogleChartAPI = 'https://chart.googleapis.com/chart?cht=qr&chs=200x200&chld=Q&chl='
+        var Conteudoqrcode = GoogleChartAPI + encodeURI(inputUsuario);
+        document.querySelector('#exitqrcode').src = Conteudoqrcode;
+    } else if (document.querySelector('#optionsDensity').value === '3') {
+        var inputUsuario = document.querySelector('textarea').value;
+        var GoogleChartAPI = 'https://chart.googleapis.com/chart?cht=qr&chs=200x200&chld=H&chl='
+        var Conteudoqrcode = GoogleChartAPI + encodeURI(inputUsuario);
+        document.querySelector('#exitqrcode').src = Conteudoqrcode;
+    }
+    else if (document.querySelector('#optionsDensity').value === '4') {
+        var inputUsuario = document.querySelector('textarea').value;
+        var GoogleChartAPI = 'https://chart.googleapis.com/chart?cht=qr&chs=200x200&chld=L&chl='
+        var Conteudoqrcode = GoogleChartAPI + encodeURI(inputUsuario);
+        document.querySelector('#exitqrcode').src = Conteudoqrcode;
+    }
 }
